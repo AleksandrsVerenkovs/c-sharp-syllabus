1c9f279ad23987699ca74f56d3c922e3f1c4de5c
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-15 21:20:00 +0300

    IF statement in one line.

--- Arithmetic/Exercise_1/Program.cs
+++ Arithmetic/Exercise_1/Program.cs
@@ -17,14 +17,7 @@ namespace Exercise_1
         }
         public static bool Compare(int num1, int num2)
         {
-           if(num1 == 15 || num2 == 15)
-            {
-                return true;
-            }
-           if(Math.Abs(num1+num2) == 15)
-            {
-                return true;
-            }
+            if (num1 == 15 || num2 == 15 || Math.Abs(num1 + num2) == 15) return true;
             return false;
         }
     }
