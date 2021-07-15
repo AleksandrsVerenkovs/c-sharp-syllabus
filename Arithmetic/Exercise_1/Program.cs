4657f575dd3b53d82080c2f58fd0464545362a5e
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-15 15:24:04 +0300

    Exercise_1

---
+++ Arithmetic/Exercise_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first;
+            int second;
+            Console.WriteLine("Enter first INTEGER nr.");
+            first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second INTEGER nr.");
+            second = int.Parse(Console.ReadLine());
+            Console.WriteLine(Compare(first, second));
+            Console.ReadKey();
+        }
+        public static bool Compare(int num1, int num2)
+        {
+           if(num1 == 15 || num2 == 15)
+            {
+                return true;
+            }
+           if(Math.Abs(num1+num2) == 15)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
