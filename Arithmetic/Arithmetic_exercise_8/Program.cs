0037601a364623772d269c4376fadbd08e95354f
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-15 22:51:19 +0300

    Exercise_8

---
+++ Arithmetic/Arithmetic_exercise_8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Arithmetic_exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int workHours;
+
+            Console.WriteLine("Enter work hours.");
+            workHours = int.Parse(Console.ReadLine());
+            Console.WriteLine(Salary(workHours));
+            Console.ReadKey();
+        } 
+        public static string Salary(int hours)
+        {
+            int hourlyRate = 8;
+            double overtimeHourlyRate = 1.5 * hourlyRate;
+            int baseSalary = hours * hourlyRate;
+            double overtimePay = (hours - 40) * overtimeHourlyRate;
+            int maxSalary = 720;
+
+            if (hours > 60) return $"You salary is {maxSalary}$.\nSorry, but not more than 60h work time.";
+            if (hours > 40 && hours <= 60) return $"You salary is {baseSalary + overtimePay}$.";
+            return $"You salary is {baseSalary}$ .";
+
+        }
+
+    }
+}
+
