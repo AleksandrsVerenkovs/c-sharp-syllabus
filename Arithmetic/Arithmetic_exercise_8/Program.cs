a326bd513913d244ea7e6a4ee76e25925c385b67
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-16 15:38:41 +0300

    Exercise_8

--- Arithmetic/Arithmetic_exercise_8/Program.cs
+++ Arithmetic/Arithmetic_exercise_8/Program.cs
@@ -1,27 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Arithmetic_exercise_8
 {
     class Program
     {
+        
+        public class Employee
+        {
+            public string Name;
+            public double BasePay;
+            public int WorkedHours;
+
+            public Employee(string name, double basePay, int workedHours)
+            {
+                Name = name;
+                BasePay = basePay;
+                WorkedHours = workedHours;
+            }
+        }
         static void Main(string[] args)
         {
-            int workHours;
 
-            Console.WriteLine("Enter work hours.");
-            workHours = int.Parse(Console.ReadLine());
-            Console.WriteLine(Salary(workHours));
+            List<Employee> Employees = new List<Employee>();
+            Employees.Add(new Employee("Employee 1", 7.50, 35));
+            Employees.Add(new Employee("Employee 2", 8.20, 47));
+            Employees.Add(new Employee("Employee 3", 10.00, 73));
+
+            foreach(var person in Employees)
+            {
+                Console.WriteLine($"{person.Name} salary: {Salary(person.WorkedHours,person.BasePay)}");
+            }
             Console.ReadKey();
         } 
-        public static string Salary(int hours)
+        public static string Salary(int hours,double hourlyRate)
         {
-            int hourlyRate = 8;
             double overtimeHourlyRate = 1.5 * hourlyRate;
-            int baseSalary = hours * hourlyRate;
+            double baseSalary = hours * hourlyRate;
             double overtimePay = (hours - 40) * overtimeHourlyRate;
             int maxSalary = 720;
 
-            if (hours > 60) return $"You salary is {maxSalary}$.\nSorry, but not more than 60h work time.";
+            if (hourlyRate < 8) return $"No, no. Hourly rate {hourlyRate} is too low!";
+            if (hours > 60) return $"You salary is {maxSalary}$. Sorry, but not more than 60h work time.";
             if (hours > 40 && hours <= 60) return $"You salary is {baseSalary + overtimePay}$.";
             return $"You salary is {baseSalary}$ .";
 
