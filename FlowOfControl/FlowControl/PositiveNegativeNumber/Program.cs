5ae6da66b9a02cbcbcc3f64655949a302eb5bbf6
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-16 16:14:14 +0300

    FlowOfControl_exercise_2

--- FlowOfControl/FlowControl/PositiveNegativeNumber/Program.cs
+++ FlowOfControl/FlowControl/PositiveNegativeNumber/Program.cs
@@ -6,23 +6,22 @@ namespace PositiveNegativeNumber
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Enter the number.");
-            var input = Console.ReadKey();
-            /*
-            fixme
-            if (?)
+
+            var input = int.TryParse(Console.ReadLine(), out int num);
+
+            if (!input)
             {
-                Console.WriteLine("Number is positive");
-            } 
-            else if (?) 
-            {
-                Console.WriteLine("Number is negative");
-            } 
-            else 
-            {
-                Console.WriteLine("Number is zero");
+                Console.WriteLine("Heeey, that's not a number!");
             }
-            */
+            else
+                {
+                if (num>= 1) Console.WriteLine("Number is positive");
+                else if (num <= -1) Console.WriteLine("Number is negative");
+                else Console.WriteLine("Number is zero");
+                }
+            Console.ReadKey();
         }
     }
 }
