51b63c362ab8f0684de67d74bd4e94a1d677f759
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-16 23:34:27 +0300

    refactor: split decoding versions to seperate classes.

---
+++ FlowOfControl/FlowControl/PhoneKeyPad/PhoneNumber2.cs
@@ -0,0 +1,61 @@
+﻿using static PhoneKeyPad.Program;
+
+namespace PhoneKeyPad
+{
+    class PhoneNumber2
+    {
+        public static int PhoneNum2(char inputChar)
+        {
+            var phoneLetters = new PhoneCode();
+
+            switch (inputChar)
+            {
+                case 'a':
+                case 'b':
+                case 'c':
+                    return 2;
+                    break;
+                case 'd':
+                case 'e':
+                case 'f':
+                    return 3;
+                    break;
+                case 'g':
+                case 'h':
+                case 'i':
+                    return 4;
+                    break;
+                case 'j':
+                case 'k':
+                case 'l':
+                    return 5;
+                    break;
+                case 'm':
+                case 'n':
+                case 'o':
+                    return 6;
+                    break;
+                case 'p':
+                case 'q':
+                case 'r':
+                case 's':
+                    return 7;
+                    break;
+                case 't':
+                case 'u':
+                case 'v':
+                    return 8;
+                    break;
+                case 'w':
+                case 'x':
+                case 'y':
+                case 'z':
+                    return 9;
+                    break;
+                default:
+                    return 0;
+                    break;
+            }
+        }
+    }
+}
---
+++ FlowOfControl/FlowControl/PhoneKeyPad/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhoneKeyPad
+{
+    class Program
+    {
+        public class PhoneCode
+        {
+         
+            public char[] ABC = {'a','b','c'};
+            public char[] DEF = { 'd', 'e', 'f' };
+            public char[] GHI = { 'g', 'h', 'i' };
+            public char[] JKL = { 'j', 'k', 'l' };
+            public char[] MNO = { 'm', 'n', 'o' };
+            public char[] PQRS = { 'p', 'q', 'r' ,'s'};
+            public char[] TUV = { 't', 'u', 'v' };
+            public char[] WXYZ = { 'w', 'x', 'y','z' };
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text (letters only).");
+            var inputArr = Console.ReadLine().ToLower().ToCharArray();
+            Console.WriteLine("Choose decoding version: 1(nested-if) 2(switch).");
+            var decodingVersion = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(DecodeTextToNumber(inputArr,decodingVersion));
+            Console.ReadKey();
+        }
+        public static string DecodeTextToNumber(char[] arr, int version)
+        {
+            List<int> PhoneDigits1 = new List<int>();
+
+            switch (version)
+            {
+                case 1:
+                    foreach (var letter in arr)
+                    {
+                        PhoneDigits1.Add(PhoneNumber1.PhoneNum(letter));
+                    }
+                    return string.Join("", PhoneDigits1);
+                    break;
+                case 2:
+                    foreach (var letter in arr)
+                    {
+                        PhoneDigits1.Add(PhoneNumber2.PhoneNum2(letter));
+                    }
+                    return string.Join("", PhoneDigits1);
+                    break;
+                default:
+                    return "Choose version 1 or 2.";
+                    break;
+            }
+        }
+    }
+}
