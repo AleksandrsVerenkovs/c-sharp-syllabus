97f5163eaa666619ffdc8e6f21fe41b4f3e7d034
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-20 16:22:28 +0300

    Loops_exercise_10 Not fineshed

---
+++ Loops/Loops/NumberSquare/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Min: ");
+            var minVal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Max : ");
+            var maxVal = int.Parse(Console.ReadLine());
+
+            //List<int> baseNumber = new List<int>();
+            var baseNum = new int[maxVal];
+
+            var increment = 0;
+            for (int j = 0; j <= maxVal; j++)
+            {
+                //if(minVal + increment > maxVal)
+                //{
+                //    increment = 0;
+                //}
+                baseNum[j] = minVal + j;
+                increment = j;
+            }
+
+            for ( int i = minVal; i<= maxVal; i++)
+            {
+                
+                Console.WriteLine(string.Join(' ',baseNum));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
