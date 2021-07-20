ba8b8078932b32223677cd48a74a869eea62ed83
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-20 13:08:04 +0300

    Loops_exercise_1 chore: did exercise

--- Loops/Loops/Exercise1/Program.cs
+++ Loops/Loops/Exercise1/Program.cs
@@ -6,10 +6,15 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            int i;
+            int i = 0;
 
             Console.WriteLine("The first 10 natural numbers are: ");
-
+            for (var j = 0; j< 10; j++)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadKey();
             /*
             todo - write a program in C# to display the first 10 natural numbers
             for (......) 
