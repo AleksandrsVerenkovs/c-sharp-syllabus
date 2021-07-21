6a5911a68134bd1f255a5d3f760ab65c990e5c65
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-21 12:24:54 +0300

    Arrays_exercise_10 chore: created Hangman game

--- Arrays/Arrays/Hangman/Program.cs
+++ Arrays/Arrays/Hangman/Program.cs
@@ -8,63 +8,64 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            var tryCount = 7;
-            HashSet<char> wrongLetter = new HashSet<char>();
-
-            var wordList = new string[5] { "mammoth", "leviathan", "sabath", "imbecile", "foxtrot" };
-
-            var wordInList = wordList[random.Next(0, 5)];
-            var wordArr = wordInList.ToCharArray();
-            var hiddenWord = new string('_', wordArr.Length).ToCharArray();
-
-            Console.WriteLine(wordInList);
-
-            while (tryCount > 0)
+            bool playAgain = false;
+            do
             {
-                Console.WriteLine(tryCount);
+                Random random = new Random();
+                var tryCount = 7;
+                HashSet<char> wrongLetter = new HashSet<char>();
 
-                Console.WriteLine($"\nWord: {string.Join(' ', hiddenWord)}\n");
-                Console.WriteLine($"Misses: {string.Join(' ', wrongLetter)}\n");
+                var wordList = new string[5] { "mammoth", "leviathan", "sabath", "imbecile", "foxtrot" };
 
-                Console.Write("Guess: ");
-                var guess = char.Parse(Console.ReadLine());
+                var wordInList = wordList[random.Next(0, 5)];
+                var wordArr = wordInList.ToCharArray();
+                var hiddenWord = new string('_', wordArr.Length).ToCharArray();
 
-                for (int i = 0; i < wordArr.Length; i++)
+                while (tryCount > 0)
                 {
-                    if (wordArr[i] == guess)
+                    Console.WriteLine($"\nWord: {string.Join(' ', hiddenWord)}\n");
+                    Console.WriteLine($"Trys left: {tryCount}");
+                    Console.WriteLine($"Misses: {string.Join(' ', wrongLetter)}\n");
+
+                    Console.Write("Guess: ");
+                    var guess = char.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < wordArr.Length; i++)
                     {
-                        hiddenWord[i] = guess;
+                        if (wordArr[i] == guess)
+                        {
+                            hiddenWord[i] = guess;
+                        }
+                    }
+
+                    if (!Array.Exists(wordArr, elem => elem == guess))
+                    {
+                        wrongLetter.Add(guess);
+                        tryCount--;
+                    }
+
+                    var isWon = Array.Exists(hiddenWord, elem => elem == '_');
+
+                    if (!isWon)
+                    {
+                        Console.WriteLine("You smart cookie!");
+                        break;
                     }
                 }
 
-                if(!Array.Exists(wordArr, elem => elem == guess))
+                if (tryCount == 0)
                 {
-                    wrongLetter.Add(guess);
-                    tryCount--;
+                    Console.WriteLine("Sorry, better luck next time!");
                 }
 
-                var isWon = Array.Exists(hiddenWord, elem => elem == '_');
-
-                if (!isWon)
+                Console.WriteLine("Play again? [y/n]");
+                var response = Console.ReadKey(true);
+                if(response.KeyChar == 'y')
                 {
-                    Console.WriteLine("yeyey");
-                    break;
+                    playAgain = playAgain == true;
                 }
-            }
-
-            if(tryCount == 0)
-            {
-            Console.WriteLine("Sorry, better luck next time!");
-            }
-
-            Console.WriteLine("Play again? [y/n]");
-            var playAgain = Console.ReadKey();
-            if(playAgain.KeyChar = 'n')
-            {
-                Application
-            }
-            Console.ReadKey();
+            } while (playAgain);
+                Console.ReadKey();
         }
     }
 }
