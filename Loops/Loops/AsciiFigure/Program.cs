9f72636065bf1d96b7d2e01d8f658649a99e8399
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-22 02:53:52 +0300

    Loops_exercise_8 chor:created method,that print pyramid

--- Loops/Loops/AsciiFigure/Program.cs
+++ Loops/Loops/AsciiFigure/Program.cs
@@ -6,7 +6,45 @@ namespace AsciiFigure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var size3 = Constant.Size3;
+            //var side = 16;
+            //var center = 0;
+            //for(var i = 0; i < 5; i++)
+            //{
+            //    Console.Write(new String('/',side));
+            //    Console.Write(new String('*', center));
+            //    Console.Write(new String('\\', side));
+            //    Console.WriteLine();
+            //    side -= 4;
+            //    center += 8;
+            //}
+            Pyramid(size3);
+
+            Console.ReadLine();
+
+            void Pyramid(int size)
+            {
+                var center = 0;
+                var side = 0;
+                for (var i = 2; i <= size; i++)
+                {
+                    side += 4;
+                }
+
+                for (var i = 0; i < size; i++)
+                {
+                    Console.Write(new String('/', side));
+                    Console.Write(new String('*', center));
+                    Console.Write(new String('\\', side));
+                    Console.WriteLine();
+                    side -= 4;
+                    center += 8;
+                }
+            }
+        }
+        class Constant
+        {
+            public const int Size3 = 3;
         }
     }
 }
