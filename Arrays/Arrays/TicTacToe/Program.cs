a5237a47fb54bc0c9d35d0a049e080b920670547
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-21 16:15:27 +0300

    Arrays_exercise_7 chore: FINALLY created TIK-TAK-TOE

--- Arrays/Arrays/TicTacToe/Program.cs
+++ Arrays/Arrays/TicTacToe/Program.cs
@@ -8,8 +8,43 @@ namespace TicTacToe
 
         private static void Main(string[] args)
         {
+            bool gameWon = false;
+
+            Random random = new Random();
+            char playerTurn = ' ';
+            playerTurn = random.Next(0, 2) == 0 ? 'X' : 'O';
+         
+            Console.WriteLine($"{playerTurn} goes first!\n");
             InitBoard();
-            DisplayBoard();
+            while (!gameWon)
+            {
+                if(Winner() == 'X' || Winner() == 'O')
+                {
+                    DisplayBoard();
+                    Console.WriteLine($"{Winner()} is winner!");
+                    break;
+                }
+                if (Tie())
+                {
+                    Console.WriteLine("It's a tie!");
+                    break;
+                }
+                DisplayBoard();
+
+                Console.Write($"{playerTurn}, choose your location (row, column): ");
+                var inputX = int.Parse(Console.ReadKey().KeyChar.ToString());
+                Console.Write(" ");
+                var inputY = int.Parse(Console.ReadKey().KeyChar.ToString());
+                Console.WriteLine();
+
+                if (board[inputX,inputY] == ' ')
+                {
+                    board[inputX, inputY] = playerTurn;
+                    playerTurn = playerTurn == 'X' ? 'O' : 'X';
+                }
+               
+            }
+            Console.ReadLine();
         }
 
         private static void InitBoard()
@@ -22,6 +57,17 @@ namespace TicTacToe
             }
             
         }
+        private static bool Tie()
+        {
+            var isTie = false;
+            // fills up the board with blanks
+            for (var r = 0; r < 3; r++)
+            {
+                for (var c = 0; c < 3; c++)
+                    isTie = board[r, c] == ' ' ? false : true;
+            }
+            return isTie;
+        }
 
         private static void DisplayBoard()
         {
@@ -31,6 +77,49 @@ namespace TicTacToe
             Console.WriteLine("    --+-+--");
             Console.WriteLine("  2  " + board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2]);
             Console.WriteLine("    --+-+--");
+            Console.WriteLine();
+        }
+        private static char Winner()
+        {
+            //Horizontal
+            if(board[0,0] == board[0,1] && board[0,1] == board[0, 2])
+            {
+                return board[0,0];
+            }
+            else if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
+            {
+                return board[1, 0];
+            }
+            else if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
+            {
+                return board[2, 0];
+            }
+            //Vertical
+            else if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0])
+            {
+                return board[0, 0];
+            }
+            else if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1])
+            {
+                return board[0, 1];
+            }
+            else if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2])
+            {
+                return board[0, 2];
+            }
+            //Diagonal
+            else if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                return board[0, 0];
+            }
+            else if (board[0, 2] == board[1, 1] && board[1, 1] == board[0, 2])
+            {
+                return board[0, 2];
+            }
+            else
+            {
+                return '-';
+            }
         }
     }
 }
