a08ee1dec4a6e2d1c4f68f1168933827c43cfea8
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-22 22:25:01 +0300

    Loop_exercise_8 style: removed comments

--- Loops/Loops/AsciiFigure/Program.cs
+++ Loops/Loops/AsciiFigure/Program.cs
@@ -7,17 +7,7 @@ namespace AsciiFigure
         static void Main(string[] args)
         {
             var size3 = Constant.Size3;
-            //var side = 16;
-            //var center = 0;
-            //for(var i = 0; i < 5; i++)
-            //{
-            //    Console.Write(new String('/',side));
-            //    Console.Write(new String('*', center));
-            //    Console.Write(new String('\\', side));
-            //    Console.WriteLine();
-            //    side -= 4;
-            //    center += 8;
-            //}
+
             Pyramid(size3);
 
             Console.ReadLine();
