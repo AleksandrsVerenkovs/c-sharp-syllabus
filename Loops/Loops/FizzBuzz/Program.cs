8e0c34cb4c07bd5ac00bf52f74d17718e3e255ed
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-22 22:48:29 +0300

    Loops_exercise6 refactor: removed 2nd cycle

--- Loops/Loops/FizzBuzz/Program.cs
+++ Loops/Loops/FizzBuzz/Program.cs
@@ -11,40 +11,23 @@ namespace FizzBuzz
 
             for(int i = 1; i <= input;i++)
             {
-                if(i % 20 == 0 && i % 15 == 0)
+                if (i % 15 == 0)
                 {
-                    Console.WriteLine("FizzBuzz ");
+                    Console.Write("FizzBuzz ");
                 }
-                else if (i % 20 == 0 && i % 5 == 0)
+                else if (i % 5 == 0)
                 {
-                    Console.WriteLine("Buzz ");
+                    Console.Write("Buzz ");
                 }
-                else if (i % 20 == 0 && i % 3 == 0)
+                else if (i % 3 == 0)
                 {
-                    Console.WriteLine("Fizz ");
+                    Console.Write("Fizz ");
                 }
                 else
                 {
-                    if (i % 15 == 0)
-                    {
-                        Console.Write("FizzBuzz ");
-                    }
-                    else if(i % 5 == 0)
-                    {
-                        Console.Write("Buzz ");
-                    }
-                    else if(i % 3 == 0)
-                    {
-                        Console.Write("Fizz ");
-                    }
-                    else
-                    {
-                        Console.Write($"{i} ");
-                    }
-
+                    Console.Write($"{i} ");
                 }
-
-
+                if (i % 20 == 0) Console.WriteLine();
             }
             Console.ReadKey();
         }
