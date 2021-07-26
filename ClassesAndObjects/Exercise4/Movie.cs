61f866b2ca09f61d521a6542dbe227c6c8c774df
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-26 13:32:23 +0300

    CAO_exercise4 chore: something done

---
+++ ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise4
+{
+    class Movie
+    {
+        private string Title;
+        private string Studio;
+        private string Rating;
+        private List<Movie> PGMovies;
+
+        public Movie(string title, string studio, string rating)
+        {
+            Title = title;
+            Studio = studio;
+            Rating = rating;
+        }
+        public Movie(string title, string studio)
+        {
+            Title = title;
+            Studio = studio;
+            Rating = "PG";
+        }
+
+        public Movie[] GetPG(List<Movie> movies)
+        {
+            return PGMovies.Where(movie => movie.Rating == "PG").ToArray();
+        }
+    }
+}
---
+++ ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CR = new Movie("Casino Royale", "Eon Productions", "PG13");
+            var Glass = new Movie("Glass", "Buena Vista International", "PG13");
+            var SM = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures");
+
+            Console.ReadKey();
+
+        }
+    }
+}
