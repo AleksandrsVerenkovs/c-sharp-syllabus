757a73e67908ae96e9b4e03fef88da01a69cb4de
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-29 11:32:19 +0300

    CAO_exercise12

--- ClassesAndObjects/VideoStore/Video.cs
+++ ClassesAndObjects/VideoStore/Video.cs
@@ -1,40 +1,50 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace VideoStore
 {
     class Video
     {
+        private string _title;
+        private bool _isAvailable;
+        private List<double> _rating;
+
+        //private double Rating { get; set; }
+        //private bool IsAvailable { get; set; }
+
         public Video(string title)
         {
-            
+            _title = title;
+            _isAvailable = true;
+            _rating = new List<double>();
         }
 
         public void BeingCheckedOut()
         {
-            
+            _isAvailable = false;
         }
 
         public void BeingReturned()
         {
-            
+            _isAvailable = true;
         }
 
         public void ReceivingRating(double rating)
         {
-            
+            _rating.Add(rating);
         }
 
         public double AverageRating()
         {
-            return 0;
+            return Enumerable.Range((int)_rating[0],_rating.Count()).Aggregate(0,(a,b) => a+b) / _rating.Count();
         }
 
         public bool Available()
         {
-            return true;
+            return _isAvailable;
         }
 
-        public string Title => "";
+        public string Title => _title;
 
         public override string ToString()
         {
