c762970a3b758381943e4cd622ad3888be4dd77a
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-07-30 12:25:01 +0300

    Collections_list_exercise1

--- Collections/ListExercise1/Program.cs
+++ Collections/ListExercise1/Program.cs
@@ -10,13 +10,16 @@ namespace ListExercise1
     {
         private static void Main(string[] args)
         {
-            //TODO: Write a C# program to create a new list, add some elements (string) and print out the collection.
             var colors = new List<string>();
 
-            //TODO: Add 5 colors to list
             colors.Add("Red");
+            colors.Add("Blue");
+            colors.Add("Green");
+            colors.Add("Yellow");
+            colors.Add("Black");
 
-            Console.WriteLine();
+            Console.WriteLine(string.Join(" ",colors));
+            Console.ReadLine();
         }
     }
 }
