8ca744045cf8956b4c8b118eccc5809dc69b2ff0
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-08-04 12:59:15 +0300

    Polymorphism_exercise1

---
+++ Polymorphism/DragRace/Volvo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragRace
+{
+    public class Volvo:ICar,IBoost
+    {
+        private int currentSpeed = 0;
+        private string Name = "Volvo";
+
+        public string CarBrand()
+        {
+            return Name;
+        }
+        public void SpeedUp()
+        {
+            currentSpeed++;
+        }
+        public void SlowDown()
+        {
+            currentSpeed--;
+        }
+        public string ShowCurrentSpeed()
+        {
+            return currentSpeed.ToString();
+        }
+        public void UseNitrousOxideEngine()
+        {
+            currentSpeed += 2;
+        }
+        public void StartEngine()
+        {
+            Console.WriteLine("FRrr...sandals..and..socks");
+        }
+    }
+}
