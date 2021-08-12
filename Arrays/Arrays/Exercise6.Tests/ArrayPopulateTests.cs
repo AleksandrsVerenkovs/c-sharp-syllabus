750e8c112297fdfde27ced0d72c952f6990c5bcb
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-08-12 13:46:55 +0300

    shait

---
+++ Arrays/Arrays/Exercise6.Tests/ArrayPopulateTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercise6.Tests
+{
+
+    [TestClass]
+    public class ArrayPopulationTests
+    {
+        Arr
+        private Arr
+        public ArrayPopulationTests()
+        {
+            _target = new 
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Arrays/Arrays/Exercise6.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Exercise6.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercise6.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a34b84f-f972-4dcb-956b-90a5a95fb16d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
