f53d11982ec1e4cfac5949ea517e440cfc668605
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-08-13 17:22:21 +0300

    Tests_Collections_exercise5 Vāds tests ir melīgs  ;)

---
+++ Collections/CollectionsTests/Exercise5Tests.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+using WordCount;
+
+namespace CollectionsTests
+{
+    public class Exercise5Tests
+    {
+        [Theory]
+        [InlineData("this counts as 16", 17)]
+        [InlineData("some text", 9)]
+        [InlineData("some text with !< sym0", 22)]
+        [InlineData("word \n or two", 13)]
+        public void CharCount_ReturNumOfChars(string text, int expected)
+        {
+            var actual = WordCount.Program.CharCount(text);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("this counts as 16", 1)]
+        [InlineData("some\n text\n", 3)]
+        [InlineData("word \n or two", 2)]
+        public void LineCount_ReturNumOfLines(string text, int expected)
+        {
+            var actual = WordCount.Program.LineCount(text);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("this counts as 16", 4)]
+        public void WordCount_ReturNumOfWord(string text, int expected)
+        {
+            var actual = WordCount.Program.WordCount(text);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
