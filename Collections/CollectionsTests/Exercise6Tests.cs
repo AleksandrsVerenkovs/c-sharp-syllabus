cce83166ef26692f28a39dcdea255ffa59ae4b00
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-08-16 16:16:40 +0300

    Tests_Collections_exercise6

---
+++ Collections/CollectionsTests/Exercise6Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using FlightPlanner;
+
+namespace CollectionsTests
+{
+    public class Exercise6Tests
+    {
+        Cities cityCreator = new Cities();
+
+        [Theory]
+        [InlineData(new string[] { "b -> dest1", "c-> dest2", "d -> dest3", "b -> dest4" }, new string[] { "b", "c", "d" })]
+        public void CityList_ReturnHashSet(string[] list, string[] expected)
+        {
+            var city = new Cities();
+            city.CityDictionary(list);
+            var actual = city.CityList();
+
+            Assert.Equal(expected, actual);
+        }
+
+        //[Theory]
+        //[InlineData(
+        //    new string[] { "b -> dest1", "c-> dest2", "d -> dest3", "b -> dest4" },
+        //    new Dictionary<string, List<string>>() {
+        //        {"b",new List<string>(){"dest1","dest4" } }
+        //    })]
+        //public void CityDictionary_ReturnDictionary(string[] baseInfo, Dictionary<string, List<string>> expected)
+        //{
+        //    var cityCreator = new Cities();
+
+        //}
+
+
+    }
+}
