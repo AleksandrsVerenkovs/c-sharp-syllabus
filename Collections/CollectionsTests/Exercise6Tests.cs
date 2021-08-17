705eedeca43567c8645fc91eca766b5bd220aa7c
EdgarsSalmins <edgars.salmins@gmail.com>
2021-08-17 12:28:47 +0300

    TestDataGenerator

--- Collections/CollectionsTests/Exercise6Tests.cs
+++ Collections/CollectionsTests/Exercise6Tests.cs
@@ -4,22 +4,51 @@ using System.Linq;
 using Xunit;
 using FlightPlanner;
 using System.Collections;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace CollectionsTests
 {
     public class TestDataGenerator : IEnumerable<object[]>
     {
-
-        public IEnumerator<string[]> GetStr()
+        public IEnumerator<object[]> GetEnumerator()
         {
-            yield return (string[])(new object[] { new string[] { "b -> dest1", "c-> dest2", "d -> dest3", "b -> dest4" } });
+            yield return new object[]
+            {
+                new[]
+                {
+                    "b -> dest1",
+                    "c-> dest2",
+                    "d -> dest3",
+                    "b -> dest4"
+                }
+            };
+            yield return new object[]
+            {
+                new[]
+                {
+                    "g -> dest5",
+                    "h-> dest6",
+                    "j -> dest7",
+                    "l -> dest8"
+                }
+            };
         }
-        IEnumerator IEnumerable.GetStr() => GetStr();
 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
     public class Exercise6Tests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
         Cities cityCreator = new Cities();
+
+        public Exercise6Tests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
 
         [Theory]
         [InlineData(new string[] { "b -> dest1", "c-> dest2", "d -> dest3", "b -> dest4" }, new string[] { "b", "c", "d" })]
@@ -37,8 +66,7 @@ namespace CollectionsTests
         public void CityDictionary_ReturnDictionary(string[] baseInfo)
         {
             var cityCreator = new Cities();
-
-
+            
         }
 
 
